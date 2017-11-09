13368b899d9226c0e0ac2fdb1cc7220afd3a65ce
Scott Lewis <scott@iconify.it>
2017-11-09 17:05:14 -0500

    Code commit

--- Ai Sessions.jsx
+++ Ai Sessions.jsx
@@ -5,25 +5,25 @@
  * 1. Place this script in Applications > Adobe Illustrator > Presets > en_US > Scripts
  * 2. Restart Adobe Illustrator to activate the script
  * 3. The script will be available under menu File > Scripts > BatchResizeArtboards
- * 4. ... 
+ * 4. ...
  */
 /**
  * LICENSE & COPYRIGHT
  *
- *   You are free to use, modify, and distribute this script as you see fit. 
- *   No credit is required but would be greatly appreciated. 
+ *   You are free to use, modify, and distribute this script as you see fit.
+ *   No credit is required but would be greatly appreciated.
  *
  *   Scott Lewis - scott@iconify.it
  *   http://github.com/iconifyit
  *   http://iconify.it
  *
  *   THIS SCRIPT IS OFFERED AS-IS WITHOUT ANY WARRANTY OR GUARANTEES OF ANY KIND.
- *   YOU USE THIS SCRIPT COMPLETELY AT YOUR OWN RISK AND UNDER NO CIRCUMSTANCES WILL 
- *   THE DEVELOPER AND/OR DISTRIBUTOR OF THIS SCRIPT BE HELD LIABLE FOR DAMAGES OF 
- *   ANY KIND INCLUDING LOSS OF DATA OR DAMAGE TO HARDWARE OR SOFTWARE. IF YOU DO 
+ *   YOU USE THIS SCRIPT COMPLETELY AT YOUR OWN RISK AND UNDER NO CIRCUMSTANCES WILL
+ *   THE DEVELOPER AND/OR DISTRIBUTOR OF THIS SCRIPT BE HELD LIABLE FOR DAMAGES OF
+ *   ANY KIND INCLUDING LOSS OF DATA OR DAMAGE TO HARDWARE OR SOFTWARE. IF YOU DO
  *   NOT AGREE TO THESE TERMS, DO NOT USE THIS SCRIPT.
  */
- 
+
 var originalInteractionLevel = userInteractionLevel;
 userInteractionLevel = UserInteractionLevel.DONTDISPLAYALERTS;
 
@@ -48,11 +48,11 @@ var session_filename = "ai-" + dateString + "-r1" + CONST.JSON_EXT;
 var session_logfile  = "ai-log-"  + dateString  + "-r1" +  CONST.TEXT_EXT;
 
 
-var dialog = new Window("dialog", "Ai Sessions", [150, 150, 500, 500]); 
+var dialog = new Window("dialog", "Ai Sessions", [150, 150, 500, 500]);
 
 // Message area
 
-dialog.msgBox = dialog.add("statictext", [30,30,300,60], ""); 
+dialog.msgBox = dialog.add("statictext", [30,30,300,60], "");
 
 // Cancel button
 
@@ -62,20 +62,20 @@ dialog.closeBtn.onClick = function() { dialog.close() };
 dialog.openBtn = dialog.add("button", [130,275,220,315], "Open", {name:"open"});
 dialog.openBtn.enabled = false;
 
-dialog.openBtn.onClick = function(){ 
+dialog.openBtn.onClick = function(){
 
     doOpenSession(CONST.SRCFOLDER + "/" + dialog.sessions.selection.text);
 };
 
 dialog.saveBtn = dialog.add("button", [230,275,320,315], "Save", {name:"save"});
-dialog.saveBtn.onClick = function(){ 
+dialog.saveBtn.onClick = function(){
 
     var button = dialog.saveBtn;
-    
+
     // The business logic
-    
+
     if (app.documents.length == 0) {
-    
+
         alert(CONST.NO_OPEN_DOCS);
     }
     else {
@@ -89,12 +89,12 @@ dialog.saveBtn.onClick = function(){
             }
             var logfolder = new Folder(CONST.LOGFOLDER);
             if (! logfolder.exists) {
-    
+
                 logfolder.create();
             }
-        
+
             var testFile = new File(CONST.SRCFOLDER + "/" + session_filename);
-        
+
             var n = 1;
             var max = 100;
             while (testFile.exists && n < max) {
@@ -102,20 +102,20 @@ dialog.saveBtn.onClick = function(){
                 testFile = new File(CONST.SRCFOLDER + "/" + session_filename);
                 n++;
             }
-        
+
             session_logfile  = "ai-log-"  + dateString  + "-r" + n + CONST.TEXT_EXT
-        
+
             logger(
                 CONST.SRCFOLDER + "/" + session_filename,
                 "{files:[\r" + "    " + openDocs.join(",\r    ") + "\r]}"
             );
-            
+
             initSessionsList(dialog);
             dialog.msgBox.text = CONST.SESSION_SAVED;
             dialog.saveBtn.enabled = false;
         }
         catch(ex) {
-    
+
             logger(CONST.LOGFOLDER + "/" + session_logfile, "ERROR: " + ex.message);
         }
         userInteractionLevel = originalInteractionLevel;
@@ -136,7 +136,7 @@ dialog.show();
 function initSessionsList(dialog) {
     var sessions = new Folder(CONST.SRCFOLDER).getFiles("*.json");
     if (! sessions.length) {
-    
+
         dialog.msgBox.text = "You have no saved sessions";
     }
     else {
@@ -149,7 +149,7 @@ function initSessionsList(dialog) {
          * Let's show the newest sessions at the top.
          */
         sessions.reverse();
-        
+
         dialog.sessions = dialog.add("listbox", [30, 70, 320, 230]);
         for (i=0; i<sessions.length; i++) {
             item = dialog.sessions.add("item", (new File(sessions[i])).name);
@@ -159,7 +159,7 @@ function initSessionsList(dialog) {
             dialog.openBtn.enabled = true;
         }
         dialog.sessions.onDoubleClick = function() {
-        
+
             dialog.openBtn.enabled = true;
             doOpenSession(CONST.SRCFOLDER + "/" + dialog.sessions.selection.text);
         }
@@ -174,15 +174,15 @@ function doOpenSession(filepath) {
     var read_file = new File(filepath);
 
     if (read_file) {
-    
+
         dialog.close();
 
         try {
             if (read_file.alias) {
                 while (read_file.alias) {
                     read_file = read_file.resolve().openDlg(
-                        CONST.CHOOSE_FILE, 
-                        txt_filter, 
+                        CONST.CHOOSE_FILE,
+                        txt_filter,
                         false
                     );
                 }
@@ -193,18 +193,16 @@ function doOpenSession(filepath) {
         }
 
         try {
-    
             read_file.open('r', undefined, undefined);
             if (read_file !== '') {
-                var _json = read_file.read();
-                var obj = eval(_json);
+                var obj = eval(read_file.read());
                 if (typeof(obj) == "object") {
-            
                     if (obj.length) {
                         for(i=0; i<obj.length; i++) {
-                
-                            //alert(obj[i]);
-                            doc = app.open(new File(obj[i]));
+                            var the_file = new File(obj[i]);
+                            if (the_file.exists) {
+                                doc = app.open(the_file);
+                            }
                         }
                     }
                 }
@@ -212,7 +210,6 @@ function doOpenSession(filepath) {
             }
         }
         catch(ex) {
-
             try { read_file.close(); }catch(ex){};
             dialog.msgBox.text = ex.message;
             logger(session_logfile, "ERROR: " + ex.message);
@@ -228,14 +225,14 @@ function doOpenSession(filepath) {
  * @param string txt         The text to write to the file
  * @return void
  */
-function logger(filepath, txt) {  
+function logger(filepath, txt) {
 
-    var file = new File(filepath);  
-    file.open("e", "TEXT", "????");  
-    file.seek(0,2);  
+    var file = new File(filepath);
+    file.open("e", "TEXT", "????");
+    file.seek(0,2);
     $.os.search(/windows/i)  != -1 ? file.lineFeed = 'windows'  : file.lineFeed = 'macintosh';
-    file.writeln(txt);  
-    file.close();  
+    file.writeln(txt);
+    file.close();
 }
 
 /**
